d09d09680b44628fc9a40ba894ebe956126aabf1
Maulidatulhasanah <lidamaulida261@gmail.com>
2021-06-11 13:15:57 +0700

    Project IDEA

---
+++ PraktikumJava1 Modul01/Modul01_IDEA/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        String namaDepan = "Maulidatul";
+        String namaBelakang = "Hasanah";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.45;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+        
+        System.out.println(namaDepan + namaBelakang);
+    }
+}
