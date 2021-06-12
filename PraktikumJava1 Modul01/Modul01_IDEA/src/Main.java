1c03f27b7683bb2121613c345549f1778fb9a5fe
Maulidatulhasanah <lidamaulida261@gmail.com>
2021-06-12 18:02:18 +0700

    Operasi Tipe String

--- PraktikumJava1 Modul01/Modul01_IDEA/src/Main.java
+++ PraktikumJava1 Modul01/Modul01_IDEA/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args){
         String namaDepan = "Maulidatul";
@@ -9,10 +11,26 @@ public class Main {
         boolean cantik = true;
 
         System.out.println(namaDepan + namaBelakang);
-        
+
         char[] unisKaChar = {'U','N','I','S','K','A'};
         String unisKaString = new String(unisKaChar);
         System.out.println(unisKaString);
+
+        String namaLengkap = namaDepan + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("tul"));
+        System.out.println(namaLengkap.substring(8));
+        System.out.println(namaLengkap.substring(8,13));
+        System.out.println(namaLengkap.replace( "tul", "Yasa"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(8));
+
+        String[] namaArray = namaLengkap.split( " ");
+
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
     }
 }
 
