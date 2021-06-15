095b7aaa1669d4e1b49cb063af554e9008a38027
Maulidatulhasanah <lidamaulida261@gmail.com>
2021-06-15 15:12:26 +0700

    Membuat Method

--- PraktikumJava1 Modul01/Modul02/src/Model/Mahasiswa.java
+++ PraktikumJava1 Modul01/Modul02/src/Model/Mahasiswa.java
@@ -16,27 +16,44 @@ public class Mahasiswa {
         System.out.println("Nama \t\t\t : " + this.nama);
         System.out.println("NPM \t\t\t : " + this.npm);
         System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(this.tanggalLahir));
+        System.out.println("Usia : \t\t\t : " + hitungUsia());
     }
 
     void menyapa() {
         System.out.println("Hai nama aku : " + this.nama);
     }
 
+    String getNama(){
+        return nama;
+    }
+
     int hitungUsia() {
         Calendar tanggalLahir = Calendar.getInstance();
         tanggalLahir.setTime(this.tanggalLahir);
-        Calendar hariIni = Calendar.getInstance();
+        Calendar tanggalIni = Calendar.getInstance();
 
-        int selisihTahun = hariIni.get(Calendar.YEAR) - tanggalLahir.get(Calendar.YEAR);
+        int tahunlLahir = tanggalLahir.get(Calendar.YEAR);
+        int tahunIni = tanggalIni.get(Calendar.YEAR);
 
-        if (hariIni.get(Calendar.MONTH) < tanggalLahir.get(Calendar.MONTH)) {
+        int selisihTahun = tahunIni - tahunlLahir;
+
+        int bulanLahir = tanggalLahir.get(Calendar.MONTH);
+        int bulanIni = tanggalIni.get(Calendar.MONTH);
+
+        if(bulanIni<bulanLahir){
             selisihTahun--;
-        } else {
-            if (hariIni.get(Calendar.MONTH) == tanggalLahir.get(Calendar.MONTH)
-                    && hariIni.get(Calendar.DAY_OF_MONTH) < tanggalLahir.get(Calendar.DAY_OF_MONTH)) {
-                selisihTahun--;
+        }else{
+            int tanggalTanggalLahir = tanggalLahir.get(Calendar.DAY_OF_MONTH);
+            int tanggalTanggalIni = tanggalIni.get(Calendar.DAY_OF_MONTH);
+            if(bulanIni == bulanLahir){
+                if(tanggalTanggalIni < tanggalTanggalLahir){
+                    selisihTahun--;
+                }
             }
         }
         return selisihTahun;
-    }
-}
+            }
+        }
+
+
+
