c9b6049cb4db7483ca9851d8f6425c387686e0bc
Maulidatulhasanah <lidamaulida261@gmail.com>
2021-06-15 20:01:16 +0700

    Contoh Perhitungan Luas

--- PraktikumJava1 Modul01/Modul03/src/GuiByText.java
+++ PraktikumJava1 Modul01/Modul03/src/GuiByText.java
@@ -8,6 +8,14 @@ public class GuiByText extends JFrame {
     JTextField textField1 = new JTextField();
     JButton button1 = new JButton("Greet");
 
+    JLabel label12 = new JLabel("Panjang");
+    JLabel label13 = new JLabel("Lebar");
+    JLabel label14 = new JLabel("Hasil");
+    JTextField tPanjang = new JTextField();
+    JTextField tLebar = new JTextField();
+    JTextField tHasil = new JTextField();
+    JButton bHitung = new JButton("Hitung Luas");
+
     GuiByText() {
         setSize(640, 480);
         setLocationRelativeTo(null);
@@ -21,21 +29,46 @@ public class GuiByText extends JFrame {
         getContentPane().add(label11);
         getContentPane().add(textField1);
         getContentPane().add(button1);
+        getContentPane().add(label12);
+        getContentPane().add(label13);
+        getContentPane().add(label14);
+        getContentPane().add(tPanjang);
+        getContentPane().add(tLebar);
+        getContentPane().add(tHasil);
+        getContentPane().add(bHitung);
 
         label11.setBounds(16, 32, 100, 25);
         textField1.setBounds(124, 32, 100, 25);
         button1.setBounds(232, 32, 80, 25);
+        label12.setBounds(16,80,100,25);
+        tPanjang.setBounds(124,80,100,25);
+        label13.setBounds(16, 113, 100, 25);
+        tLebar.setBounds(124, 113, 100,25);
+        bHitung.setBounds(16, 146, 208, 25);
+        label14.setBounds(16, 179, 100, 25);
+        tHasil.setBounds(124, 179, 100, 25);
     }
+
 
     void menambahkanListener() {
         button1.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                String nama = textField1.getText();
+            public void actionPerformed(ActionEvent actionEvent){
+                String nama = textField1.getText()
                 JOptionPane.showMessageDialog(null, "Hai" + nama);
             }
         });
+        bHitung.addActionListener(actionEvent -> {
+            int panjang = Integer.parseInt(tPanjang.getText());
+            int lebar = Integer.parseInt(tLebar.getText());
+
+            int hasil = panjang * lebar;
+            String sHasil = String.valueOf(hasil);
+
+            tHasil.setText(sHasil);
+        });
     }
+
     public static void main(String[] args){
         GuiByText guiByText = new GuiByText();
         guiByText.meletakkanKomponen();
