fa15e5923df78745c34cac3c3c4ec9ec408d0e02
Maulidatulhasanah <lidamaulida261@gmail.com>
2021-06-15 19:04:13 +0700

    Menambahkan Event Listener

--- PraktikumJava1 Modul01/Modul03/src/GuiByText.java
+++ PraktikumJava1 Modul01/Modul03/src/GuiByText.java
@@ -1,27 +1,42 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuiByText extends JFrame {
 
     JLabel label11 = new JLabel("Coba Input 1");
     JTextField textField1 = new JTextField();
+    JButton button1 = new JButton("Greet");
 
-    GuiByText(){
+    GuiByText() {
         setSize(640, 480);
         setLocationRelativeTo(null);
         setTitle("GUI By Text");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    void meletakkanKomponen(){
+    void meletakkanKomponen() {
         getContentPane().setLayout(null);
 
         getContentPane().add(label11);
         getContentPane().add(textField1);
+        getContentPane().add(button1);
 
-        label11.setBounds(16, 32, 100,25);
-        textField1.setBounds(124,32,100,25);
+        label11.setBounds(16, 32, 100, 25);
+        textField1.setBounds(124, 32, 100, 25);
+        button1.setBounds(232, 32, 80, 25);
     }
-    public static void main(String[] args) {
+
+    void menambahkanListener() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String nama = textField1.getText();
+                JOptionPane.showMessageDialog(null, "Hai" + nama);
+            }
+        });
+    }
+    public static void main(String[] args){
         GuiByText guiByText = new GuiByText();
         guiByText.meletakkanKomponen();
         guiByText.setVisible(true);
